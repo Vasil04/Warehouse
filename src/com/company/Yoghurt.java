fd484384c2984a5e2b3c5e52542aa1cd66605a7a
Nikola Nikolaev <nikola_nikolaev1@abv.bg>
2020-05-12 14:53:12 +0300

    Created Yoghurt class

---
+++ src/com/company/Yoghurt.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Yoghurt extends Product{
+
+    public Yoghurt(String name, String description, String expiryDate, String entryDate, String nameOfMaker, String weight, int availability, String location, String comment, int maxCountOfProductOnOneShelf) {
+        super(name, description, expiryDate, entryDate, nameOfMaker, weight, availability, location, comment, maxCountOfProductOnOneShelf);
+    }
+}
