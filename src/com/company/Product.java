7abcf9c19b85928fae0efc32c38f47d381dc82cb
Vasil <vdvasilev04@gmail.com>
2020-05-12 11:48:34 +0300

    added Product and Warehouse

---
+++ src/com/company/Product.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Product {
+    public String name, description, expirationDate, dateOfDeliveryInWarehouse, makerName, unit, availability, position, comment;
+    public int maxProductCountOnOneShelf;
+
+}
