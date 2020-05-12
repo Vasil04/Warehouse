22b0cd7970885951931eb832b5c26c7ec423a279
Vasil <vdvasilev04@gmail.com>
2020-05-12 12:52:26 +0300

    updated Product and Warehouse

--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -4,4 +4,16 @@ public class Product {
     public String name, description, expirationDate, dateOfDeliveryInWarehouse, makerName, unit, availability, position, comment;
     public int maxProductCountOnOneShelf;
 
+    public Product(String name, String description, String expirationDate, String dateOfDeliveryInWarehouse, String makerName, String unit, String availability, String position, String comment, int maxProductCountOnOneShelf) {
+        this.name = name;
+        this.description = description;
+        this.expirationDate = expirationDate;
+        this.dateOfDeliveryInWarehouse = dateOfDeliveryInWarehouse;
+        this.makerName = makerName;
+        this.unit = unit;
+        this.availability = availability;
+        this.position = position;
+        this.comment = comment;
+        this.maxProductCountOnOneShelf = maxProductCountOnOneShelf;
+    }
 }
--- src/com/company/Warehouse.java
+++ src/com/company/Warehouse.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class Warehouse {
+    public Product array[][] = new Product[10][10];
+
+
+    public void placeProduct (Product product, int row, int column, int neededSpace) {
+        for (int i = 0; i < neededSpace; i++) {
+            array[column][row] = product;
+            row++;
+        }
+
+    }
 }
