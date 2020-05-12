6e61219edaeb67e5a5a9443b1332136ebec5f240
Nikola Nikolaev <nikola_nikolaev1@abv.bg>
2020-05-12 14:36:57 +0300

    Updated Product class

--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -1,80 +1,122 @@
 package com.company;
 
 public class Product{
+
     private String name;
     private String description;
     private String expiryDate;
-    private String EntryDate;
-    private String NameOfMaker;
-    private String Weight;
-    private String Location;
-    private int MaxCount;
-    public Product(String name,String description,String expiryDate,String EntryDate,String NameOfMaker,String Weight,String Location,int MaxCount){
-        this.SetName(name);
-        this.SetDescription(description);
-        this.SetExpiryDate(expiryDate);
-        this.SetEntryDate(EntryDate);
-        this.SetNameOfMaker(NameOfMaker);
-        this.SetWeight(Weight);
-        this.SetMaxCount(MaxCount);
+    private String entryDate;
+    private String nameOfMaker;
+    private String weight;
+    private int availability;
+    private String location;
+    private String comment;
+    private int maxCountOfProductOnOneShelf;
+
+    public Product(String name, String description, String expiryDate, String entryDate, String nameOfMaker, String weight, int availability, String location, String comment, int maxCountOfProductOnOneShelf) {
+        this.name = name;
+        this.description = description;
+        this.expiryDate = expiryDate;
+        this.entryDate = entryDate;
+        this.nameOfMaker = nameOfMaker;
+        this.weight = weight;
+        this.availability = availability;
+        this.location = location;
+        this.comment = comment;
+        this.maxCountOfProductOnOneShelf = maxCountOfProductOnOneShelf;
     }
+
     public Product(){
-        this.SetName("1");
-        this.SetDescription("2");
-        this.SetExpiryDate("expiryDate");
-        this.SetEntryDate("EntryDate");
-        this.SetNameOfMaker("NameOfMaker");
-        this.SetWeight("Weight");
-        this.SetMaxCount(10);
+        this.name = "Test";
+        this.description = "Testing";
+        this.expiryDate = "13/06/2021";
+        this.entryDate = "12/05/2020";
+        this.nameOfMaker = "test.inc";
+        this.weight = "kg";
+        this.availability =10;
+        this.location="something";
+        this.comment="Nothing to say";
+        this.maxCountOfProductOnOneShelf = 4;
 
     }
-    public void SetName(String val){
-        this.name = val;
-    }
-    public String GetName(){
-        return this.name;
-    }
-    public void SetDescription(String val){
-        this.description = val;
-    }
-    public String GetDescription(){
-        return this.description;
-    }
-    public void SetExpiryDate(String val){
-        this.expiryDate = val;
-    }
-    public String GetExpiryDate(){
-        return this.expiryDate;
-    }
-    public void SetEntryDate(String val){
-        this.EntryDate = val;
-    }
-    public String GetEntryDate(){
-        return this.EntryDate;
-    }
-    public void SetNameOfMaker(String val){
-        this.NameOfMaker = val;
-    }
-    public String GetNameOfMaker(){
-        return this.NameOfMaker;
-    }
-    public void SetWeight(String val){
-        this.Weight = val;
-    }
-    public String GetWeight(){
-        return this.Weight;
-    }
-    public void SetLocation(String val){
-        this.Location = val;
-    }
-    public String GetLocation(){
-        return this.Location;
-    }
-    public void SetMaxCount(int val){
-        this.MaxCount = val;
-    }
-    public int GetMaxCount(){
-        return this.MaxCount;
+
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(String entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public String getNameOfMaker() {
+        return nameOfMaker;
+    }
+
+    public void setNameOfMaker(String nameOfMaker) {
+        this.nameOfMaker = nameOfMaker;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(int availability) {
+        this.availability = availability;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getMaxCountOfProductOnOneShelf() {
+        return maxCountOfProductOnOneShelf;
+    }
+
+    public void setMaxCountOfProductOnOneShelf(int maxCountOfProductOnOneShelf) {
+        this.maxCountOfProductOnOneShelf = maxCountOfProductOnOneShelf;
+    }
 }
